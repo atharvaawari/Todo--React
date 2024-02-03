d170909cd1f1c3c7f04118f9347204d9ccc2cedb
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2024-02-03 20:43:56 +0530

    working on Todoitem component

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,19 @@ function App() {
     setTodos((prev)=> prev.filter((todo)=> todo.id !== id))
   }
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo)=> prevTodo === id ? {...prevTodo, completed: !prevTodo.completed } : prevTodo) )
+    setTodos((prev) => prev.map((prevTodo)=> prevTodo === id ? {...prevTodo, completed: !prevTodo.completed } : prevTodo))
   }
 
-  useEffect( ()=>{
-    const todos =JSON.parse(localStorage.getItem("todos"))
-    if( todos && todos.length > 0) {
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length >0 ) {
       setTodos(todos)
     }
-  },[] )
-
-  useEffect( ()=>{
-    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [])
+  
+  useEffect(()=>{
+    localStorage.setItem( "todos", JSON.stringify(todos))
   }, [todos])
 
   return (
